43bc4dc8c06f7c9fbb5c181a15078b0bef6e8ec8
MikolajPietron <pietron.mikolaj@gmail.com>
2025-07-26 13:36:01 +0200

    Possible Fix

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,10 @@
-// backend/api/index.js
 const serverless = require('serverless-http');
-const app = require('../server');
+const express = require('express');
+
+const app = express();
+
+app.get('/api/getgamescore', (req, res) => {
+  res.json([{ playerName: "Test", score: 100, date: new Date() }]);
+});
 
 module.exports.handler = serverless(app);
